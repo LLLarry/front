d908406b061002ea32b195bb0498e6f51613762f
LLLarry <1079485906@qq,com>
2022-08-22 14:22:53 +0800

    完成移动端顶部导航栏部分

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,35 @@ export const getSecret = () => {
   const icode = md5(`${codetype}LGD_Sunday-1991`)
   return { codetype, icode }
 }
+
+export const scrollTransition = () => {
+  let timer = null
+  return function exec ({el = document.body, position = 0, direction = 'v',  time = 150} = options) {
+    clearInterval(timer)
+    // 每步的时间 ms
+    const TIME_EVERY_STEP = 5 
+    // 最大滚动距离
+    const maxScrollSize = el.scrollWidth - el.offsetWidth
+    // 限定position的有效滚动范围
+    position = Math.max(Math.min(position, maxScrollSize), 0)
+    // 可以分为多少步
+    let steps = Math.ceil(time / TIME_EVERY_STEP)
+    const stepSize = (position - el.scrollLeft) / steps // 每步的长度
+    
+    timer = setInterval(() => {
+      // console.log(el.scrollLeft , position)
+      if (el.scrollLeft !== Number.parseInt(position) && position >= 0) {
+        if (stepSize >= 0) {
+          let scrollX = el.scrollLeft + stepSize >= position ? position :  el.scrollLeft + stepSize
+          el.scrollLeft = scrollX
+        } else {
+          let scrollX = el.scrollLeft + stepSize <= position ? position :  el.scrollLeft + stepSize
+          el.scrollLeft = scrollX
+        }
+        
+      } else {
+        clearInterval(timer)
+      }
+    }, TIME_EVERY_STEP)
+  }
+}
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,6 +1,6 @@
 <template>
   <ul
-    class="relative z-10 text-xs bg-white flex overflow-auto p-1 text-zinc-600"
+    class="relative z-10 text-xs bg-white flex overflow-auto p-1 text-zinc-600 pr-6"
     ref="ulEle"
   >
     <li
@@ -10,19 +10,26 @@
     <li
       v-for="(category, index) in data"
       :key="category.id"
-      class="shrink-0 px-1.5 py-0.5 last:mr-6 z-10"
+      class="shrink-0 px-1.5 py-0.5 z-10"
       :class="{ 'text-zinc-50': index === curretIndex }"
       @click="handleSelectCategory(index)"
       :ref="storeLiEle"
     >
       {{ category.name }}
     </li>
+    <li class="shrink-0 px-1.5 py-0.5 z-10">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </li>
+    <li
+      class="px-1 w-4 h-4 fixed top-0 bottom-0 right-[-2px] z-20 shadow-l-white bg-white"
+    >
+      <svg-icon
+        name="hamburger"
+        @click="visible = true"
+        class="px-1 w-4 h-4 fixed top-0 right-[-2px] z-20 shadow-l-white bg-white"
+      />
+    </li>
   </ul>
-  <svg-icon
-    name="hamburger"
-    class="px-1 w-4 h-4 fixed top-0 right-[-2px] z-20 shadow-l-white bg-white"
-    @click="visible = true"
-  />
   <popup v-model="visible" class="aaa" style="color: red">
     <Menu :categorys="data" @handleSelectCategory="handleSelectCategory" />
   </popup>
@@ -32,6 +39,8 @@
 import { ref, watch, nextTick } from 'vue'
 import { useScroll } from '@vueuse/core'
 import Menu from '@/views/main/components/menu/index.vue'
+import { scrollTransition } from '@/utils'
+const run = scrollTransition()
 const props = defineProps({
   data: {
     type: Array,
@@ -43,7 +52,8 @@ const props = defineProps({
 const curretIndex = ref(-1)
 const sliderStyle = ref({
   left: '10px',
-  width: '0px'
+  width: '0px',
+  bottom: 0
 })
 //  ul dom元素
 const ulEle = ref(null)
@@ -61,7 +71,13 @@ const visible = ref(false)
 const handleSelectCategory = (index) => {
   curretIndex.value = index
   visible.value = false
-  ulEle.value.scrollTo(scrollRaces.value[index], 0)
+  // ulEle.value.scrollTo(scrollRaces.value[index], 0)
+  run({
+    el: ulEle.value,
+    position: scrollRaces.value[index],
+    direction: 'l',
+    time: 200
+  })
 }
 // 获取v-for遍历的子元素dom节点时，需要使用回调函数获取; 注意: 每次页面更新之后storeLiEle，都会重新执行一遍，这样会导致liEles中存储的都是重复的元素
 // 所以可以使用Set来存储数据，避免存入重复的数据， 也可以在obBeforeUpdate前设置liEles.value的值为初始化值
@@ -69,21 +85,26 @@ const storeLiEle = (el) => {
   liEles.value.add(el)
 }
 
-watch(curretIndex, (newIndex, oldIndex) => {
-  // 保证渲染之后再进行计算元素的位置
-  nextTick(() => {
-    // 获取点击元素的距离左边屏幕的距离和元素的宽度
-    const liEle = Array.from(liEles.value)[newIndex]
-    if (!liEle) return false
-    const { left, width } = liEle.getBoundingClientRect()
-    sliderStyle.value = {
-      left: `${left + ulScrollLeft.value}px`,
-      width: `${width}px`
-    }
-  })
-}, {
-  immediate: true
-})
+watch(
+  curretIndex,
+  (newIndex, oldIndex) => {
+    // 保证渲染之后再进行计算元素的位置
+    nextTick(() => {
+      // 获取点击元素的距离左边屏幕的距离和元素的宽度
+      const liEle = Array.from(liEles.value)[newIndex]
+      if (!liEle) return false
+      const { left, width, height } = liEle.getBoundingClientRect()
+      sliderStyle.value = {
+        left: `${left + ulScrollLeft.value}px`,
+        width: `${width}px`,
+        height: `${height}px`
+      }
+    })
+  },
+  {
+    immediate: true
+  }
+)
 
 // 监听data初次数据渲染之后，将slider条设置到第一项
 watch(
@@ -94,17 +115,21 @@ watch(
       curretIndex.value = 0
       // 获取1/2屏幕的宽度
       const halfScreenWidth = window.innerWidth / 2
-      // 每一项向左滚动的距离 = 每一项距离屏幕左边的距离 - 1/2屏幕的宽度 + 1/2自身的宽度 
-      scrollRaces.value = Array.from(liEles.value).map(el => el.getBoundingClientRect().left - halfScreenWidth + el.offsetWidth / 2)
+      // 每一项向左滚动的距离 = 每一项距离屏幕左边的距离 - 1/2屏幕的宽度 + 1/2自身的宽度
+      scrollRaces.value = Array.from(liEles.value).map(
+        (el) =>
+          el.getBoundingClientRect().left - halfScreenWidth + el.offsetWidth / 2
+      )
     })
-  }, {
+  },
+  {
     immediate: true
   }
 )
 </script>
 
 <style scoped>
-ul {
+/* ul {
   scroll-behavior: smooth;
-}
+} */
 </style>
